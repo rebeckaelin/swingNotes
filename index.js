6d5f607026a22f0d20c54b5b326e594f6b3f1fb1
rebeckaelin <rebeckaelin@hotmail.com>
2024-04-29 18:29:06 +0200

    added a swagger doc

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const user = require("./user");
 const notes = require("./notes");
 
+const swaggerUI = require("swagger-ui-express");
+const APIdocs = require("./docs/docs.json");
+app.use("/api/docs", swaggerUI.serve);
+app.get("/api/docs", swaggerUI.setup(APIdocs));
+
 app.use(express.json());
 app.use("/api", user);
 app.use("/api", notes);
